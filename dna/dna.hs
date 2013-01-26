5bf9610d2f4fcc584851805e2475e4c0ea11c198
Kamil Kisiel <kamil@kamilkisiel.net>
2013-01-26 00:34:02 -0800

    dna in Haskell

---
+++ dna/dna.hs
@@ -0,0 +1,14 @@
+import Data.Char
+
+countChars (a, c, g, t) 'A' = (a + 1, c, g, t)
+countChars (a, c, g, t) 'C' = (a, c + 1, g, t)
+countChars (a, c, g, t) 'G' = (a ,c, g + 1, t)
+countChars (a, c, g, t) 'T' = (a ,c, g, t + 1)
+countChars (a, c, g, t)  _  = (a ,c, g, t)
+
+
+printTuple (a, c, g, t) = show a ++ " " ++ show c ++ " " ++ show g ++ " " ++ show t ++ "\n"
+
+main = do
+    contents <- getContents
+    putStr (printTuple (foldl countChars (0, 0, 0, 0) (map toUpper contents)))
